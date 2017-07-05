acd0a069ae65091635a9eeaf5ed26d8840688784
yeyingtomorrow <5193928@qq.com>
2017-07-05 14:58:24 +0800

    add url link

---
+++ wooyun wiki/企业安全/1-服务配置/server_directory-list [WooYun WiKi]_files/css.php
@@ -0,0 +1,25 @@
+
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+<meta http-equiv="x-ua-compatible" content="ie=7"/>
+<title> 升级中 </title>
+
+<body>
+
+<div style="text-align:center">
+<img  src='http://www.wooyun.org/notice.png'/>
+</div>
+</body>
+</html>
+
+
+
+/*
+     FILE ARCHIVED ON 07:40:38 Jul 26, 2016 AND RETRIEVED FROM THE
+     INTERNET ARCHIVE ON 06:52:50 Jul 05, 2017.
+     JAVASCRIPT APPENDED BY WAYBACK MACHINE, COPYRIGHT INTERNET ARCHIVE.
+
+     ALL OTHER CONTENT MAY ALSO BE PROTECTED BY COPYRIGHT (17 U.S.C.
+     SECTION 108(a)(3)).
+*/
---
+++ wooyun wiki/企业安全/1-服务配置/server_resolve [WooYun WiKi]_files/js.php
@@ -0,0 +1,25 @@
+
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+<meta http-equiv="x-ua-compatible" content="ie=7"/>
+<title> 升级中 </title>
+
+<body>
+
+<div style="text-align:center">
+<img  src='https://web.archive.org/web/20160727004004/http://www.wooyun.org/notice.png'/>
+</div>
+</body>
+</html>
+
+
+
+/*
+     FILE ARCHIVED ON 00:40:04 Jul 27, 2016 AND RETRIEVED FROM THE
+     INTERNET ARCHIVE ON 06:51:25 Jul 05, 2017.
+     JAVASCRIPT APPENDED BY WAYBACK MACHINE, COPYRIGHT INTERNET ARCHIVE.
+
+     ALL OTHER CONTENT MAY ALSO BE PROTECTED BY COPYRIGHT (17 U.S.C.
+     SECTION 108(a)(3)).
+*/
